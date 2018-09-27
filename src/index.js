183f012d96538e0380c394581a1ee195997abf06
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-27 03:20:00 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -18,14 +18,49 @@ export class OlderCoaster extends Component {
   //   )
   // }
   //
+  render(){
+    return (
+      <div class="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+
+      
+    )
+  }
 }
 
 export class InFrontOfYou extends Component {
   // your code here
+  render(){
+    return (
+      <div>
+    <p>You shouldn&#x27;t look too far.</p>
+    <p>Sometimes, the solution is right in front of you.</p>
+  </div>
+      
+    )
+  }
 }
 
 export class ButcherShop extends Component {
   // your code here
+render(){
+return(  <div class="butcher-shop">
+  <p>Hello! We have the following products for sale today:</p>
+  <ul>
+    <li>Tenderloin</li>
+    <li>Short ribs</li>
+    <li>Beef shin</li>
+    <li>Ribeye</li>
+  </ul>
+</div>
+)
+}
 }
 
 
